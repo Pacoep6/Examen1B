65e5c4a9d03e6002bb60c6e8f796ad43bb121e31
Paco Elviro <pacoep6@gmail.com>
2022-06-10 14:05:05 +0200

    Versión inicial

---
+++ src/entornosrecu2/EntornosRecu2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entornosrecu2;
+
+public class EntornosRecu2 {
+
+    public static void main(String[] args) {
+        Matematicas calc1 = new Matematicas(11);//ejemplo
+        System.out.println(calc1.capicua()); //ejemplo
+        System.out.println(calc1.multiplicar(3));
+        System.out.println(calc1.factorial());
+        
+        //aquí sólo se han programado ejemplos. Puedes hacer los tuyos
+           
+        
+        
+    }
+    
+}
---
+++ src/entornosrecu2/Matematicas.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entornosrecu2;
+
+
+public class Matematicas {
+
+    private int numero;
+
+    public Matematicas(int numIn) {
+        if ((numIn > 999) || (numIn < 1)) {
+            this.numero = 1;
+            System.out.println("Número inválido. Se establece 1");
+        } else {
+            this.numero = numIn;
+        }
+    }
+
+    public boolean capicua() {
+        if (this.numero < 10) {
+            return true;
+        } else if (this.numero < 100) {
+            if ((Math.floor(this.numero / 10)) == (this.numero % 10)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (this.numero < 1000) {
+            if ((Math.floor(this.numero / 100)) == (this.numero % 10)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+    
+    public double factorial() {
+        double result = 1;
+        for (int i = 1; i < this.numero; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public int multiplicar(int num2){
+        return (this.numero * num2);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+}
