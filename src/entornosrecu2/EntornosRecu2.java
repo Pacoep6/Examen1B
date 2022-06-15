1576cb2741444822fa4a42b8adbf626363d0f86f
Ruslan Tejerina <ruslan.tejerina@iesjulianmarias>
2022-06-15 10:18:28 +0200

    version definitiva Ruslan

--- src/entornosrecu2/EntornosRecu2.java
+++ src/entornosrecu2/EntornosRecu2.java
@@ -3,13 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package entornosrecu2;
+    /**
+     * 
+     * @author Ruslan Tejerina
+     * @version 1.0
+     * @see Matematicas#factorial() 
+     */
 
 public class EntornosRecu2 {
 
     private int numRamas; // Son 3: main, master y ramaPaco
-    private String ramaPrincipal; // master
+    private String ramaPrincipal; // main
     private String ramaEvolucionada; //es la ramaPaco
-    
+    private String explicacionEJ5; // No he visto que el programa de mi ramaRuslan en la que estoy posicinado ha cambiado nada al traer aqui la ramaPaco mas evolucionada
+    private String explicacionEJ6; // Diferencias En rojo aparece lo que habia con la version mas antigua 
+    //y en verde lo que hemos añadido en una revision mas reciente, en mi caso aparecen en verde nuevas 
+    //variables y un comentario nuevo. En la linea 13: habia  private String ramaEvolucionada; //es la ramaPaco
     public static void main(String[] args) {
         
         //aquí sólo hacemos ejemplos para ver que funciona bien la clase Matemáticas
--- src/entornosrecu2/Matematicas.java
+++ src/entornosrecu2/Matematicas.java
@@ -3,20 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package entornosrecu2;
-
+    /**
+     * 
+     * @author Ruslan Tejerina
+     * @version 1.0
+     */
 
 public class Matematicas {
+    /**
+     * Aqui se indican los atributos privados de la clase
+     */
 
     private int numero;
 
-    public Matematicas(int numIn) {
-        if ((numIn > 999) || (numIn < 1)) {
+
+    /**
+     * Constructor de la clase Matematicas, inicializa y valida el numero introducido.
+     * @param numEntrante
+     */
+    public Matematicas(int numEntrante) {
+        if ((numEntrante > 999) || (numEntrante < 1)) {
             this.numero = 1;
             System.out.println("Número inválido. Se establece 1");
         } else {
-            this.numero = numIn;
+            this.numero = numEntrante;
         }
     }
+    /**
+     * Metodo capicua 
+     * Nos devuelve un booleano indicando si un num. es capicua o no
+     * @return 
+     */
 
     public boolean capicua() {
         if (this.numero < 10) {
@@ -38,7 +55,10 @@ public class Matematicas {
         }
 
     }
-    
+    /**
+     * Metodo que devuelve el factorial de un num.
+     * @return 
+     */
     public double factorial() {
         double result = 1;
         for (int i = 1; i < this.numero; i++) {
@@ -46,15 +66,27 @@ public class Matematicas {
         }
         return result;
     }
-
+    /**
+     * Metodo Multiplicar al que se le pasa por parametro un num.
+     * y devuelve el resultado de multiplicar num2 = 3 * numero = 1
+     * @param num2
+     * @return 
+     */
     public int multiplicar(int num2){
         return (this.numero * num2);
     }
+    /**
+     * Metodo getter que devuelve un numero
+     * @return 
+     */
 
     public int getNumero() {
         return numero;
     }
-
+    /**
+     * Metodo setter al que pasamos un num. por parametro
+     * @param numero 
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
