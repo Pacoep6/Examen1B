cfc06c02c91d518d4b1e495e5c463f9a8bff8d7d
jesus sanchez <jesussanchezcrespo1@gmail.com>
2022-06-15 10:14:35 +0200

    Variable explicacion6

--- src/entornosrecu2/EntornosRecu2.java
+++ src/entornosrecu2/EntornosRecu2.java
@@ -10,6 +10,7 @@ public class EntornosRecu2 {
     private String ramaPrincipal="main"; //rellena desde el ejercicio 1!!
     private String ramaEvolucionada="main"; //rellena desde el ejercicio 1!!
     private String explicacion5="Observo que las variables no estan inicalizadas como en esta rama";
+    private String explicacion6="En la rama mas antigua sollo hay un main, en cambo en la otra rama hay variables declaradas a parte del main";
     public static void main(String[] args) {
         
         //aquí sólo hacemos ejemplos para ver que funciona bien la clase Matemáticas
