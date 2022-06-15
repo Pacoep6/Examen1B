d3a9cf9217a093b9ed3bb97d3f9f98d683b81137
MiguelAngel <chikisantamar@gmail.com>
2022-06-15 10:10:35 +0200

    revision sobre el ejercicio 8

--- src/entornosrecu2/Matematicas.java
+++ src/entornosrecu2/Matematicas.java
@@ -4,17 +4,25 @@
  */
 package entornosrecu2;
 
+import java.util.Scanner;
+
 public class Matematicas {
+    Scanner sc = new Scanner(System.in);
 
     private int numero;
     private String usuario = "Paco";
 
-    public Matematicas(int numIn) {
-        if ((numIn > 999) || (numIn < 1)) {
+    /**
+     * lo he hecho la refactorizacion a traves de la funcionalidad de netbeans de refactor-rename
+     * @param numEntrante 
+     */
+    
+    public Matematicas(int numEntrante) {
+        if ((numEntrante > 999) || (numEntrante < 1)) {
             this.numero = 1;
             System.out.println("Número inválido, " + this.usuario + ". Se establece 1");
         } else {
-            this.numero = numIn;
+            this.numero = numEntrante;
         }
     }
 
@@ -58,5 +66,20 @@ public class Matematicas {
     public void setNumero(int numero) {
         this.numero = numero;
     }
+    
+    /**
+     * metodo para refactorizar el constructor
+     * @param num 
+     */
+    public void chearNum(int num){
+        System.out.println("dame un numero");
+        numero = sc.nextInt();
+        if ((numero > 999) || (numero < 1)) {
+            //this.numero = 1;
+            System.out.println("Número inválido, " + this.usuario + ". Se establece 1");
+    }else{
+            this.numero = num;
+        }
 
+}
 }
